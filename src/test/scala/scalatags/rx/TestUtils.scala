c5ff4a8850e9210b33f8c8475d9e79ab5a2196a3
Alexander Johnston <Aliics@hotmail.com>
2022-06-05 21:12:21 +1200

    remove string test with unexpected behaviour
    scalatags cls attribute's apply method works differently now.
    When applying a new class, it is additive. I removed this broken test
    because of the odd behaviour.

--- src/test/scala/scalatags/rx/TestUtils.scala
+++ src/test/scala/scalatags/rx/TestUtils.scala
@@ -14,13 +14,7 @@ object TestUtils {
   def testRx[S, T](v: Var[S], fn: => T, initial: T, newValue: (S, T)): Unit = {
     assert(fn == initial)
     v() = newValue._1
-    
-    // scalarx is asynchronous in nature. Check a few times if given a false negative.
-    def hasChanged(rt: Int): Boolean = {
-      fn == newValue._2 || rt > 0 && hasChanged(rt - 1)
-    }
-
-    hasChanged(3)
+    assert(fn == newValue._2)
   }
 
 }
